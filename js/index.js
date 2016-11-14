e500eb6e16d7096a2ef5fa06aaa18daa7739d4e9
Brandon Mailhiot <brandon.mailhiot@gmail.com>
2016-11-14 11:00:53 -0500

    refactored

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-let starwars_melody =
+const Offbeat = require('../lib/Offbeat'),
+
+starwars_melody =
 `t g3, t g3, t g3,
 h c4, h g4,
 
@@ -9,9 +11,9 @@ t f4, t e4, t d4,
 h c5, q g4,
 
 t f4, t e4, t f4,
-h d4`
+h d4`,
 
-let ghostbusters_melody =
+ghostbusters_melody =
 `s c4, s c4, e e4, e c4, e d4, e b_3,
 q rest, q rest,
 s c4, s c4, s c4, s c4,
@@ -20,30 +22,22 @@ e b_3, e c4, q rest,
 s c4, s c4, e e4, e c4, e d4, e b_3,
 q rest, q rest,
 s c4, s c4, s c4, s c4,
-e b_3, e d4, q c4`
+e b_3, e d4, q c4`,
 
-const starwars = {
+starwars = Offbeat.layer({
   tempo: 132,
   notes: starwars_melody
-}
+}),
 
-const ghostbusters = {
+ghostbusters = Offbeat.layer({
   tempo: 120,
   notes: ghostbusters_melody
-}
-
-const starwars_time = Offbeat.time(starwars)
-const ghostbusters_time = Offbeat.time(ghostbusters)
-
-document.querySelector('#starwars button.play-btn').addEventListener('click', () => {
-  Offbeat.play(starwars)
 })
 
-document.querySelector('#ghostbusters button.play-btn').addEventListener('click', () => {
-  Offbeat.play(ghostbusters)
-})
+document.querySelector('#starwars button.play-btn').addEventListener('click', () => { starwars.play() })
+document.querySelector('#ghostbusters button.play-btn').addEventListener('click', () => { ghostbusters.play() })
 
 document.querySelector('#starwars textarea').value = starwars_melody
 document.querySelector('#ghostbusters textarea').value = ghostbusters_melody
-document.querySelector('#starwars p.time').innerHTML = '<strong>Time:</strong> ' + starwars_time.toFixed(2) + ' seconds'
-document.querySelector('#ghostbusters p.time').innerHTML = '<strong>Time:</strong> ' + ghostbusters_time.toFixed(2) + ' seconds'
+document.querySelector('#starwars p.time').innerHTML = '<strong>Time:</strong> ' + starwars.time().toFixed(2) + ' seconds'
+document.querySelector('#ghostbusters p.time').innerHTML = '<strong>Time:</strong> ' + ghostbusters.time().toFixed(2) + ' seconds'
