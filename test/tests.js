2136caa044ffcf2d9b22c8897b817aaee25da793
Brandon Mailhiot <brandonmailhiot@Brandons-MacBook-Air.local>
2017-02-03 17:33:30 -0500

    added test cases

--- test/tests.js
+++ test/tests.js
@@ -1,58 +1,84 @@
 'use strict'
 const assert = require('chai').assert,
-Offbeat = require('../lib/Offbeat.js')
+Offbeat = require('../lib/Offbeat.js'),
+AudioContext = require('web-audio-api').AudioContext
 
-describe('Offbeat declared without melody object', () => {
+describe('Offbeat declared with defaults', () => {
   describe('#time()', () => {
     it('should return the default time', () => {
-      const time = new Offbeat().time()
+      const melody = new Offbeat({ context: AudioContext })
+      let time = melody.time()
       assert.equal(time, 0)
     })
   })
+
   describe('#to_time(1)', () => {
     it('should return 4 seconds', () => {
-      const time = new Offbeat().to_time(1)
+      const melody = new Offbeat({ context: AudioContext })
+      let time = melody.to_time(1)
       assert.equal(time, 4)
     })
   })
 })
 
-describe('Offbeat declared with melody object', () => {
-  describe('#parse_notes(notes)', () => {
+
+describe('Offbeat declared with custom object', () => {
+  describe('#parse_notes()', () => {
     it('should return array of notes', () => {
-      const layer = new Offbeat({
-        notes: `a b, c d, e f`
-      }),
-      notes = layer.parse_notes(layer.notes)
-      assert.deepEqual(notes, [['a','b'],['c','d'],['e','f']])
+        const melody = new Offbeat({
+          notes: `a b, c d, e f`,
+          context: AudioContext
+        })
+        let notes = melody.notesParsed
+        assert.deepEqual(notes, [['a','b'],['c','d'],['e','f']])
     })
   })
+
   describe('#time()', () => {
     it('should return time of audio in seconds', () => {
-      const time = new Offbeat({
+      const melody = new Offbeat({
         tempo: 120,
         timeSig: '12/8',
-        notes: 't a1, t a1, t a1'
-      }).time().toFixed(2)
+        notes: 't a1, t a1, t a1',
+        context: AudioContext
+      })
+      let time = melody.time().toFixed(2)
       assert.equal(time, 1.50)
     })
   })
+
   describe('#to_time(1)', () => {
     it('should return 6 seconds', () => {
-      const time = new Offbeat({
-        tempo: 120,
-        timeSig: '12/8',
-        notes: 't a1, t a1, t a1'
-    }).to_time(1)
-      assert.equal(time, 6)
+        const melody = new Offbeat({
+            tempo: 120,
+            timeSig: '12/8',
+            notes: 't a1, t a1, t a1',
+            context: AudioContext
+        })
+        let time = melody.to_time(1)
+        assert.equal(time, 6)
     })
   })
-  describe('#queryKey()', () => {
-    it('should return C Major', () => {
+
+  describe('#brief()', () => {
+    it('should return unique, sorted, origin notes', () => {
       const melody = new Offbeat({
-        notes: 'q c3, q d3, q e3, q f3, q g3, q a3, q b3'
+        notes: 'q c3, q d3, q e3, q f3, q g3, q a3, q b3',
+        context: AudioContext
       })
-      assert.equal(melody.queryKey(), 'c major')
+      let brief = melody.brief()
+      assert.deepEqual(brief, ['c1', 'd1', 'e1', 'f1', 'g1', 'a1', 'b1'])
+    })
+  })
+
+  describe('#countNotes()', () => {
+    it('should return count of all notes', () => {
+      const melody = new Offbeat({
+        notes: 'q c3, q d3, q e3, q f3, q g3, q a3, q b3',
+        context: AudioContext
+      })
+      let count = melody.countNotes()
+      assert.deepEqual(count, {'a3' : 1, 'b3' : 1, 'c3' : 1, 'd3' : 1, 'e3' : 1, 'f3' : 1, 'g3' : 1})
     })
   })
 })
