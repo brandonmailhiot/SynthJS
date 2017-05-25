fcb0616a70b30cebabb7a24bb142db1ee293a7a5
Brandon Mailhiot <brandonmailhiot@Brandons-MacBook-Air.local>
2017-05-24 20:13:27 -0700

    dot syntax

--- js/bundle.js
+++ js/bundle.js
@@ -390,16 +390,16 @@ const ts = 0.03125
 module.exports = {
     'ts': ts, //thirty-second
     's': ts*2, //sixteenth
-    's-dot': ts*3, //dotted sixteenth
+    's.': ts*3, //dotted sixteenth
     't': ts*(8/3),  //eighth-note triplet
     'e': ts*4, //eighth
-    'e-dot': ts*6, //dotted eighth
+    'e.': ts*6, //dotted eighth
     'q': ts*8, //quarter
-    'q-dot': ts*12, //dotted quarter
+    'q.': ts*12, //dotted quarter
     'h': ts*16, //half
-    'h-dot': ts*24, //dotted half
+    'h.': ts*24, //dotted half
     'w': ts*32, //whole
-    'w-dot': ts*48 //dotted whole
+    'w.': ts*48 //dotted whole
 }
 
 },{}],4:[function(require,module,exports){
